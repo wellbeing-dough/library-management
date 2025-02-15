b694af6bd0ef0212376caf10292710c332c0c455
wellbeing-dough <kdw990202@naver.com>
2025-02-16 01:02:55 +0900

    스웨거 설정 https로 변경

--- src/main/java/com/librarymanagement/common/config/SwaggerConfig.java
+++ src/main/java/com/librarymanagement/common/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.librarymanagement.common.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -12,10 +13,15 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("도서관리 시스템 API Docs").version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("https://kimdongwoo.store").description("Production Server")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
