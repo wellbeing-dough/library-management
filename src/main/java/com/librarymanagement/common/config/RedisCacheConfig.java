02b4a476f5a24439db5c7f8235382347b10f78d2
wellbeing-dough <kdw990202@naver.com>
2025-02-16 16:56:25 +0900

    유저 단건 조회에 캐싱 적용

---
+++ src/main/java/com/librarymanagement/common/config/RedisCacheConfig.java
@@ -0,0 +1,49 @@
+package com.librarymanagement.common.config;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+@RequiredArgsConstructor
+public class RedisCacheConfig {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public RedisCacheManager userCacheManager() {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(createCacheConfig(Duration.ofMinutes(10)))
+                .build();
+    }
+
+    private RedisCacheConfiguration createCacheConfig(Duration ttl) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(ttl)
+                .disableCachingNullValues()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(createJsonSerializer()));
+    }
+
+    private GenericJackson2JsonRedisSerializer createJsonSerializer() {
+        objectMapper.activateDefaultTyping(
+                objectMapper.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
+}
