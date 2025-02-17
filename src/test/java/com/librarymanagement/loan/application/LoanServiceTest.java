641338608b8b23ebc3078a9e8f53a1d26d5f0e2f
wellbeing-dough <kdw990202@naver.com>
2025-02-17 16:53:33 +0900

    실패하는 테스트 코드 배포

--- src/test/java/com/librarymanagement/loan/application/LoanServiceTest.java
+++ src/test/java/com/librarymanagement/loan/application/LoanServiceTest.java
@@ -22,38 +22,38 @@ class LoanServiceTest {
     @Autowired
     private LoanRepository loanRepository;
 
-//    @Test
-//    @Disabled
-//    void 동시에_100개의_요청으로_책_대출을_시도하면_단_한_번만_대출이_성공한다() throws InterruptedException {
-//        // given
-//        Long bookId = 1L; // 테스트용 책 ID
-//        Long userId = 1L; // 테스트용 사용자 ID
-//
-//        int threadCount = 100;
-//        ExecutorService executorService = Executors.newFixedThreadPool(32);
-//        CountDownLatch latch = new CountDownLatch(threadCount);
-//        AtomicInteger exceptionCount = new AtomicInteger(0);  // AtomicInteger를 사용하여 예외 발생 횟수를 저장
-//
-//        // when
-//        for (int i = 0; i < threadCount; i++) {
-//            executorService.submit(() -> {
-//                try {
-//                    loanService.borrowBook(bookId, userId);  // 대출 시도
-//                } catch (Exception e) {
-//                    exceptionCount.incrementAndGet();  // 예외 발생 시 카운트 증가
-//                } finally {
-//                    latch.countDown();
-//                }
-//            });
-//        }
-//
-//        latch.await();  // 모든 쓰레드가 종료될 때까지 기다림
-//
-//        Long count = loanRepository.countByBookId(bookId);
-//
-//        assertEquals(1, count);
-//        // 예외 발생 횟수는 99여야 하므로, 예외 발생 횟수를 확인
-//        assertEquals(99, exceptionCount.get());  // 예외 발생 횟수는 99여야 함
-//    }
+    @Test
+    @Disabled
+    void 동시에_100개의_요청으로_책_대출을_시도하면_단_한_번만_대출이_성공한다() throws InterruptedException {
+        // given
+        Long bookId = 1L; // 테스트용 책 ID
+        Long userId = 1L; // 테스트용 사용자 ID
+
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        AtomicInteger exceptionCount = new AtomicInteger(0);  // AtomicInteger를 사용하여 예외 발생 횟수를 저장
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    loanService.borrowBook(bookId, userId);  // 대출 시도
+                } catch (Exception e) {
+                    exceptionCount.incrementAndGet();  // 예외 발생 시 카운트 증가
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();  // 모든 쓰레드가 종료될 때까지 기다림
+
+        Long count = loanRepository.countByBookId(bookId);
+
+        assertEquals(1, count);
+        // 예외 발생 횟수는 99여야 하므로, 예외 발생 횟수를 확인
+        assertEquals(99, exceptionCount.get());  // 예외 발생 횟수는 99여야 함
+    }
 
 }
